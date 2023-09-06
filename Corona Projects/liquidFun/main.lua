8ccf8aafccad1d18eb85f7cddf61493aee6df198
marciodasilva83 <marcinhoqp@gmail.com>
2023-09-06 18:26:23 -0300

    correção

--- Corona Projects/liquidFun/main.lua
+++ Corona Projects/liquidFun/main.lua
@@ -1,7 +1,7 @@
 local physics = require ("physics")
 physics.start()
 physics.setGravity (0, 9.8)
-physics.setDrawMode ("hybrid")
+physics.setDrawMode ("normal")
 
 display.setStatusBar (display.HiddenStatusBar)
 
@@ -12,29 +12,28 @@ local paredeEsquerda = display.newRect (-54-letterboxWidth, display.contentHeigh
 physics.addBody (paredeEsquerda, "static")
 paredeEsquerda.rotation = 86
 
-local chao = display.newRect (display.contentCenterX, display.contentHeight + 60 +letterboxHeight, 400, 120)
+local chao = display.newRect (display.contentCenterX, display.contentHeight + 60 + letterboxHeight, 440, 120)
 physics.addBody (chao, "static")
 
-local paredeDireita = display.newRect (display.contentWidth + 54 + letterboxWidth, display.contentHeight-180, 600, 70)
+local paredeDireita = display.newRect (display.contentWidth +54 +letterboxWidth, display.contentHeight-180, 600, 70)
 physics.addBody (paredeDireita, "static")
 paredeDireita.rotation = -86
 
 local bg1 = display.newImageRect ("imagens/background.png", 320, 480)
 bg1.x, bg1.y = 160, 240
 bg1.xScale = 1.202
-bg1.xScale = 1.200
-transition.to (bg1, {time=12000, x=-224,
-interations=0})
+bg1.yScale = 1.200
+transition.to (bg1, {time=12000, x=-224, iterations=0})
 
 local bg2 = display.newImageRect ("imagens/background.png", 320, 480)
 bg2.x, bg2.y = 544, 240
 bg2.xScale = 1.202
 bg2.yScale = 1.200
-transition.to (bg2, {time=12000, x=160,
-interations=0})
+transition.to (bg2, {time=12000, x=160, iterations=0})
 
 local heroi = display.newImageRect ("imagens/hero.png", 64, 64)
-heroi.x, heroi,y = 160, -400,
+heroi.x, heroi.y = 160, -400
+-- Density == densidade (acima de 1 boia, abaico de 1 afunda)
 physics.addBody (heroi, {density=0.7, friction=0.3, bounce=0.2, radius=30})
 
 local function moverHeroi (event)
@@ -55,27 +54,57 @@ local function moverHeroi (event)
             event.target:setLinearVelocity (0,0)
             event.target.angularVelocity= 0
             body.tempJoint:removeSelf()
-            body.isFixedRotation = false
+            body.isFixedRotation = false -- Deve ser comentado caso não queira que o herói gire dentro da água
         end
     end
     return true
 end
 heroi:addEventListener ("touch", moverHeroi)
 
+-- Criar um sistema de partículas para a água
+local particulas = physics.newParticleSystem {
+    filename= "imagens/liquidParticle.png",
+    radius=2,
+    imageRadius= 2,
+    gravityScale= 1.0, -- Escala de gravidade
+    strictContactCheck= true -- Checagem de contato ativada
+}
 
+-- Criar um bloco de água
+particulas:createGroup (
+    {
+        flags= {"tensile", "water"}, -- tradição
+        x=160,
+        y=0,
+        color={0.1, 0.1, 0.7, 0.5},
+        halfWidth= 128,
+        halfHeight= 256
+    }
+)
 
+-- Inicializa a tela cheia instantânea.
+local snapshot = display.newSnapshot (320 + letterboxWidth + letterboxWidth, 480 + letterboxHeight + letterboxHeight)
+local snapshotGroup = snapshot.group
+snapshot.x, snapshot.y = 160, 240
+snapshot.canvasMode = "discard"
+snapshot.alpha = 0.3
 
+-- Aplica um filtro sobel para retornar a superfície visível da água.
+snapshot.fill.effect = "filter.sobel"
 
+-- Inclui o sistema de partículas dentro do snapshot.
+snapshotGroup:insert (particulas)
+snapshotGroup.x = -160
+snapshotGroup.y = -240
 
+-- Trás o herói para frente.
+heroi:toFront()
 
+local function onEnterFrame (event)
+    snapshot:invalidate()
+end
 
-
-
-
-
-
-
-
+Runtime:addEventListener ("enterFrame", onEnterFrame)
 
 
 
--- Corona Projects/particulas/main.lua
+++ Corona Projects/particulas/main.lua
@@ -7,46 +7,52 @@ display.setStatusBar (display.HiddenStatusBar)
 local chao = display.newRect (display.contentCenterX, 470, 500, 50)
 physics.addBody (chao, "static")
 
-local paredeEsq = display.newRect (-30, display.contentCenterY, 50, 500)
-physics.addBody (chao)
+local paredeEsq = display.newRect (-10, display.contentCenterY, 50, 500)
+physics.addBody(paredeEsq, "static")
 
-local testParticula = physics.newParticleSystem (
+local testeParticula = physics.newParticleSystem (
     {
-        -- Nome do arquivo de particula
-        filename ="imagens/liquidParticle.png",
-        -- radius físico da particula
-        radius = 2,
-        -- radius da imagem (usar sempre valor maior que o radius da párticula para que elas se sebreponham e tragam um efeito visual mais satisfatório.)
+        -- Nome do arquivo de partícula
+        filename = "liquidParticle.png",
+        -- radius físico da partícula
+        radius= 2,
+        -- radius da imagem (usar sempre valor menor que o radius da partícula para que elas se sobreponham e traga um efeito visual mais satisfatório.)
         imageRadius = 4
     }
 )
 
 local function onTimer (event)
     testeParticula:createParticle (
-    { 
+    { -- Determina onde a nova partícula é gerada.
         x= 0,
-        y=0,
+        y= 0,
+        -- Valores iniciais de velocidade para a partícula.
         velocityX= 256,
         velocityY= 480,
+        -- Define a cor da partícula RGBA (A= alpha)
         color= {1, 0.2, 0.4, 1},
-        lifetime= 32.0,
+        -- Define o tempo de vida da partícula, quantos segundos ela permanece na tela antes de "morer" (0 = infinito)
+        lifeTime= 32.0,
+        -- Define o comportamento da partícula.
         flags= {"water", "colorMixing"}
     })
 end
 timer.performWithDelay (20, onTimer, 0)
 
-    testeParticula:createGroup (
+testeParticula:createGroup (
     {
         x=50,
         y=0,
         color={0, 0.3, 1, 1},
+        -- Metade da largura do grupo
         halfWidth= 64,
+        -- Metade da altura do grupo
         halfHeight = 32,
         flags ={"water", "colorMixing"}
     }
 )
 testeParticula:applyForce (0, -9.8*testeParticula.particleMass)
 
-local parede2 = display.newRect (320, display.contentCenterY, 50, 500)
-physic.addBody (parede2, "statis")
 
+local paredeDir = display.newRect (320, display.contentCenterY, 50, 500)
+physics.addBody (paredeDir, "static")
