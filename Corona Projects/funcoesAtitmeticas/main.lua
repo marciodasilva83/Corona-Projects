12b24bfc8f7c29f720a27f3412187e643bab104a
marciodasilva83 <marcinhoqp@gmail.com>
2023-09-06 19:12:56 -0300

    Update main.lua

--- Corona Projects/funcoesAtitmeticas/main.lua
+++ Corona Projects/funcoesAtitmeticas/main.lua
@@ -1,54 +1,44 @@
--- variavel global. (Deve ser evitado)
+-- Variável global. (Deve ser evitado)
 vidas = 2
 vidas = 8
 print (vidas)
-print ("o valor de vidas: " .. vidas)
------------------
--- variaveis locais (só é lida dentro do script)
--- (local) é utilizado somente na criação da variável.
-local pontos = 10 
+print ("O valor de vidas:" .. vidas)
+----------------
+-- Variáveis locais (só é lida dentro do script)
+-- (local) é utilizado somente na criação da variável
+local pontos = 10
 print (pontos)
 print ("O valor de pontos: " .. pontos)
 
----- operacoes aritmeticas ----
---- soma
---- (local significa que a variável é local) laranjas (nome da variável) = (atribuir valor) 10 (valor atribuido a variável)
-
+---------- Operações aritméticas ------------
+-- Soma
+-- local (significa que a variável é local) laranjas (nome da variável) = (atribuir valor) 10 (valor atribuído a variável)
 local laranjas = 10
 
 laranjas = laranjas + 5
+-- print imprime a informação dentro do console
 print (laranjas)
-print ( "O valor da soma das laranjas é: " .. laranjas)
+
 local bananas = 5
 
----soma entre variaveis
+-- Soma entre variáveis 
 local cesta = 0
 cesta = laranjas + bananas
-print ("a soma entre as variaveis laranjas e bananas é: " .. cesta)
+print ("A soma entre as variáveis laranjas e bananas é: " .. cesta)
 
-print (" Na sua cesta possuem ".. cesta .. " frutas")
+print ("Na sua cesta possuem " .. cesta .. " frutas")
 
-----subtracao----
+-- Subtração
+
 cesta = laranjas - bananas
-bananas = bananas -1
+bananas = bananas - 1
 
------multiplicacão----
+-- Multiplicação
 
 local carro = 8
 carro = carro * 2
-print ("quantidade de carros: " .. carro)
+print ("Quantidade de carros: " .. carro)
 
----- Dividinto pela multiplicaçao
-carro = carro * 0,5
-print ("divisao pela multiplicacao: " .. carro)
-
--- Divisão
-local gato = 6
-gato = gato / 2
-print (" A divisão final é: " .. gato)
-
-local arvore = 97.5
-arvore = arvore / 4
-print ("divisao com virgula: " .. arvore)
-
-
+-- Dividindo pela multiplicação
+carro = carro * 0.5
+print ("Divisão pela multiplicação: " .. carro)
